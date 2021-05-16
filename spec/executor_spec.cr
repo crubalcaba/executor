6ec4984eb34ea896234d081cd3a8d82dd32280fa
stark <stark9337@gmail.com>
2021-05-16 19:58:04 +0200

    style: fixed format

--- spec/executor_spec.cr
+++ spec/executor_spec.cr
@@ -2,10 +2,8 @@ require "./spec_helper"
 require "../src/executor"
 
 describe "Executor" do
-
   it "returns 200 OK on HEAD / " do
     head "/"
     response.status_code.should eq 200
   end
-
 end
